62ec31819af2fe5fa50c3c586095810bc1c975c1
DajeongLee <dajeong@fluenty.co>
2019-02-27 17:03:06 +0900

    geolocation

---
+++ project/src/js/geolocation.js
@@ -0,0 +1,63 @@
+//function that retrieves the position
+function showPosition(position) {
+  console.log('showPosition')
+  var location = {
+    longitude: position.coords.longitude,
+    latitude: position.coords.latitude
+  }
+  console.log(location);
+  showInMap(position)
+}
+
+function locationError(error) {
+	console.log('locationError')
+  console.log(error)
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            return "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            return "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            return "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            return "An unknown error occurred."
+            break;
+    }
+}
+
+function showInMap(pos) {
+    var latlon = pos.coords.latitude + "," + pos.coords.longitude;
+    var api_key = "your key";
+    var img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=14&scale=1&size=400x300&maptype=roadmap&key=${api_key}&format=png&visual_refresh=true&markers=size:tiny%7Ccolor:0xff0000%7Clabel:1%7C${latlon}`;
+
+    var map = document.querySelector("#mapholder");
+    map.innerHTML = "<img src='"+img_url+"'>";
+}
+
+//request for location
+//function that gets the location and returns it
+function getLocation() {
+  console.log('start getLocation');
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(
+			function (position) {
+				//do succes handling
+				showPosition(position)
+			},
+			function errorCallback(error) {
+				//do error handling
+				locationError(error)
+			},
+			{
+				maximumAge: Infinity,
+				timeout: 5000,
+				enableHighAccuracy: true
+			}
+		);
+	}
+}
+
+getLocation();
--- project/src/sw.js
+++ project/src/sw.js
@@ -5,9 +5,24 @@ if (workbox) {
 
   workbox.precaching.precacheAndRoute([]);
 
+  /*
+	workbox.routing.registerRoute(
+		/(.*)index.html/,
+		workbox.strategies.networkFirst({
+			cacheName: 'index-cache',
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 50,
+					maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+				})
+			]
+		})
+	);
+	*/
+
   workbox.routing.registerRoute(
 		/(.*)articles(.*)\.(?:png|gif|jpg)/,
-		workbox.strategies.cacheFirst({
+		workbox.strategies.networkFirst({
 			cacheName: 'images-cache',
 			plugins: [
 				new workbox.expiration.Plugin({
@@ -51,7 +66,7 @@ if (workbox) {
 		});
 	});
 
-	const postHandler = workbox.strategies.cacheFirst({
+	const postHandler = workbox.strategies.networkFirst({
 		cacheName: 'post-cache',
 		plugins: [
 			new workbox.expiration.Plugin({
